eecee73960703a41bc4a5445ad515415d6e37b10
Svetlozar Georgiev <sfe7lio@gmail.com>
2019-04-22 20:10:23 +0100

    most used languages are now displayed on the projects page, also added an initial contact page

--- api.py
+++ api.py
@@ -1,22 +1,15 @@
 import requests
 import json
 import os
-from TOKEN import bearer_token
+from collections import Counter
+from collections import OrderedDict
+from TOKEN import bearer_token  
 
 headers = {"Authorization": bearer_token}
 
 
-# sends a request with the requests library
-def run_query(query):
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
-        
 # github GraphQL query to get data about pinned repos
-query = """
+query_repos = """
 {
   repositoryOwner(login: "Ligh7bringer") {
     ... on User {
@@ -42,7 +35,62 @@ query = """
 }
 """
 
-result = run_query(query)
+query_langs = """
+{
+  user(login: "Ligh7bringer") {
+    repositories(first: 100) {
+      edges {
+        node {
+          languages(first: 100) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
 
-with open('data/projects.json', 'w') as outfile:  
-    json.dump(result, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
+def count_langs(data):
+  result = {}
+  for k in data:    
+    if 'name' in k:
+      result[k['name']] = result.get(k['name'], 0) + 1
+
+  return result
+
+
+# sends a request with the requests library
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+if __name__ == "__main__":
+  # get pinned repos
+  result_repos = run_query(query_repos)
+  with open('data/projects.json', 'w') as outfile:  
+      json.dump(result_repos, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
+  # get most used languages
+  result_langs = run_query(query_langs)
+  formatted = []
+  for k in result_langs['data']['user']['repositories']['edges']:
+    formatted.extend(k['node']['languages']['nodes'])
+  count = count_langs(formatted)
+
+  final = []
+  for i in count:
+    tmp = {}
+    tmp["name"] = i
+    tmp["used"] = count[i]
+    final.append(tmp)
+    
+  with open('data/languages.json', 'w') as outfile:  
+      json.dump(final, outfile, indent=4, separators=(',', ': '))
